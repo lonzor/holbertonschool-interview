5233e437cf8b1a8fbe4a3268d51283c6d0d4547c
Lonzo Rust <rustlonzo@gmail.com>
2022-03-12 01:42:56 +0000

    Debug Round 1

--- 0x00-rain/0-rain.py
+++ 0x00-rain/0-rain.py
@@ -8,15 +8,23 @@ def rain(walls):
     """
     Calculates amount of rainfall walls can keep.
     """
-    if not walls:
+    if walls is None or len(walls) < 2:
         return 0
 
     tot_rain = 0
+    l = len(walls)
+    left = [0] * l
+    right = [0] * l
 
-    for w1, w2 in enumerate(walls):
-        left_max = max(walls[:w1 + 1])
-        right_max = max(walls[w1:])
-        wall_min = min(left_max, right_max)
-        tot_rain = tot_rain + max(wall_min - w2, 0)
+    left[0] = walls[0]
+    for i in range(1, l):
+        left[i] = max(left[i - 1], walls[i])
+
+    right[l - 1] = walls[l - 1]
+    for i in range(l - 2, -1, -1):
+        right[i] = max(right[i + 1], walls[i])
+
+    for i in range(0, l):
+        tot_rain += min(left[i], right[i]) - walls[i]
 
     return tot_rain
